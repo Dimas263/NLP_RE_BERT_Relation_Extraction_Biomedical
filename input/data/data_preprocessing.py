2e6c6e4b9f1cf58e8f12e6757fb1d8921cd6df1f
research dimas <research.dimas@gmail.com>
2022-06-06 22:29:33 +0000

    Replace data_preprocessing.py

--- input/data/data_preprocessing.py
+++ input/data/data_preprocessing.py
@@ -3,35 +3,33 @@ import json
 import pandas as pd
 from pprint import pprint
 
-df = pd.read_excel('gold-standard-corpus.xlsx')
+df = pd.read_excel('input/data/gold-standard-corpus.xlsx')
 relations = list(df['relation'].unique())
 
-relations.remove('Negative')
-relation_dict = {'Negative': 0}
+relations.remove('Cause_of_disease')
+relation_dict = {'Cause_of_disease': 0}
 relation_dict.update(dict(zip(relations, range(1, len(relations) + 1))))
 
-with open('rel_dict.json', 'w', encoding='utf-8') as h:
+with open('input/data/rel_dict.json', 'w', encoding='utf-8') as h:
     h.write(json.dumps(relation_dict, ensure_ascii=False, indent=2))
 
 pprint(df['relation'].value_counts())
 
 print("============================")
 print('total data : %s' % len(df))
-print("\n")
+# print("\n")
 
 df['rel'] = df['relation'].apply(lambda x: relation_dict[x])
 
 texts = []
 
-print(" Example Data")
-print("""
-id_relation, <e1>entity1</e1>, <e2>entity2</e2>, sentence, start_entity1, end_entity1, start_entity2, end_entity2\n\n
-""")
+# print(" Example Data")
+# print("id_relation, <e1>entity1</e1>, <e2>entity2</e2>, sentence, start_entity1, end_entity1, start_entity2, end_entity2\n\n")
 
 for per1, per2, text, label, e1start, e1end, e2start, e2end in zip(
         df['plant'].tolist(),
         df['disease'].tolist(),
-        df['sentence'],
+        df['sentence'].tolist(),
         df['rel'].tolist(),
         df['e1start'].tolist(),
         df['e1end'].tolist(),
@@ -45,18 +43,21 @@ df = pd.DataFrame(texts, columns=['text', 'rel'])
 df['length'] = df['text'].apply(lambda x: len(x))
 df = df[df['length'] <= 128]
 
-predict_df = df.sample(frac=0.8, random_state=1024)
+predict_df = df.sample(frac=0.4, random_state=1024)
 train_df = df.sample(frac=0.8, random_state=1024)
 test_df = df.drop(train_df.index)
 
-with open('predict.txt', 'w', encoding='utf-8') as f:
+with open('input/data/predict.txt', 'w', encoding='utf-8') as f:
     for text, rel in zip(predict_df['text'].tolist(), predict_df['rel'].tolist()):
         f.write(str(rel) + '\t' + text + '\n')
+print ("\nsuccess to create predict.txt")
 
-with open('train.txt', 'w', encoding='utf-8') as f:
+with open('input/data/train.txt', 'w', encoding='utf-8') as f:
     for text, rel in zip(train_df['text'].tolist(), train_df['rel'].tolist()):
         f.write(str(rel) + '\t' + text + '\n')
+print ("success to create train.txt")
 
-with open('test.txt', 'w', encoding='utf-8') as g:
+with open('input/data/test.txt', 'w', encoding='utf-8') as g:
     for text, rel in zip(test_df['text'].tolist(), test_df['rel'].tolist()):
         g.write(str(rel) + '\t' + text + '\n')
+print ("success to create test.txt")
