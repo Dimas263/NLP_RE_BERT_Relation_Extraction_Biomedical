9631b616ddfa78c54ba635cb418c63325559c455
research dimas <research.dimas@gmail.com>
2022-06-06 21:10:53 +0000

    Upload New File

---
+++ output/visualization/biobert-train-chart.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import csv
+
+x = []
+y = []
+
+with open('output/visualization/biobert-train.csv', 'r') as csvfile:
+    lines = csv.reader(csvfile, delimiter=',')
+    for row in lines:
+        x.append(int(row[2]))
+        y.append(float(row[3]))
+
+plt.plot(x, y, color='g', linestyle='dashed',
+         marker='o', label="Training Loss")
+
+plt.xticks(rotation=25)
+plt.xlabel('Step')
+plt.ylabel('Loss')
+plt.title('Training', fontsize=20)
+plt.grid()
+plt.legend()
+# plt.show()
+plt.savefig('output/visualization/biobert-train.png')
