3fe5c12de16a44060936ad63f18801bd4f85031c
research dimas <research.dimas@gmail.com>
2022-06-06 21:11:48 +0000

    Upload New File

---
+++ output/visualization/BiomedNLP-PubMedBERT-dev-chart.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import csv
+
+x = []
+y = []
+
+url = 'output/visualization/BiomedNLP-PubMedBERT-dev.csv'
+
+with open(url, 'r') as csvfile:
+    lines = csv.reader(csvfile, delimiter=',')
+    for row in lines:
+        x.append(float(row[4]))
+        y.append(float(row[2]))
+
+plt.plot(x, y, color='g', linestyle='None',
+         marker='o', label="Dev Accuracy")
+
+plt.xticks(rotation=25)
+plt.xlabel('F-1')
+plt.ylabel('Accuracy')
+plt.title('Development', fontsize=20)
+plt.grid()
+plt.legend()
+# plt.show()
+plt.savefig('output/visualization/BiomedNLP-PubMedBERT-dev.png')
